7b42d6e17ce7cac0369ea0646bf6b14005265940
Mohnish <mohnishsinha011@gmail.com>
2020-10-18 12:40:34 +0530

    update Product class

--- src/main/java/com/example/Monitor/Product.java
+++ src/main/java/com/example/Monitor/Product.java
@@ -96,7 +96,7 @@ public class Product {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        //if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
         return pid == product.pid;
     }
